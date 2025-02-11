3baef56046f51f26b3d125818ed29731535f6f50
Harry Santoso <harrysantoso8@yahoo.com>
2025-02-11 15:38:38 +0700

    adding clear button filter

--- app/View/Components/TextInput.php
+++ app/View/Components/TextInput.php
@@ -14,10 +14,9 @@ class TextInput extends Component
     public function __construct(
         public ?string $value = null,
         public ?string $name = null,
-        public ?string $placeholder = null
-    ) {
-
-    }
+        public ?string $placeholder = null,
+        public ?string $formId = null
+    ) {}
 
     /**
      * Get the view / contents that represent the component.
