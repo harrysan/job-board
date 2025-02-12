f1a8d96ccf77300797ad1fd5ad5ee18f4757687a
Harry Santoso <harrysantoso8@yahoo.com>
2025-02-12 15:46:45 +0700

    refactor filter & add query scopes

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,13 +1,17 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel Job Board</title>
-        @vite('resources/css/app.css')
-    </head>
-    <body class="mx-auto mt-10 max-w-2xl bg-slate-200 text-slate-700">
-        {{ $slot }}
-    </body>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel Job Board</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body
+    class="from-10% via-30% to-90% mx-auto mt-10 max-w-2xl bg-gradient-to-r from-indigo-100 via-sky-100 to-emerald-100 text-slate-700">
+    {{ $slot }}
+</body>
+
 </html>
