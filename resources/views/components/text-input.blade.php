b81950cd74d5c34df2f9222f99b02cb36e96c538
harrysan <harrysantoso8@yahoo.com>
2025-02-13 10:17:42 +0700

    sign-in form

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -1,6 +1,6 @@
 <div class="relative">
     @if ($formRef)
-        <button type="button" class="absolute top-0 right-0 flex h-full items-center pr-2"
+        <button type="button" class="absolute top-0 right-0 flex items-center h-full pr-2"
             @click="$refs['input-{{ $name }}'].value = ''; $refs['{{ $formRef }}'].submit();">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                 stroke="currentColor" class="size-4 text-slate-500">
@@ -8,7 +8,7 @@
             </svg>
         </button>
     @endif
-    <input x-ref="input-{{ $name }}" type="text" placeholder="{{ $placeholder }}" name="{{ $name }}"
-        value="{{ $value }}" id="{{ $name }}"
+    <input x-ref="input-{{ $name }}" type="{{ $type }}" placeholder="{{ $placeholder }}"
+        name="{{ $name }}" value="{{ $value }}" id="{{ $name }}"
         class="pr-8 w-full rounded-md border-0 py-1.5 px-2.5 text-sm ring-1 ring-slate-300 placeholder:text-slate-400 focus:ring-2" />
 </div>
